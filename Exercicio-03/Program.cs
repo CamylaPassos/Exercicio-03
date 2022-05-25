3942285f2c048b6f3f0a985738f0cda0214f4513
Myla <Myla@DESKTOP-V9A76BG>
2022-05-25 19:25:36 -0300

    Add project files.

---
+++ Exercicio-03/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int abase;
+int altura;
+int resultado;
+
+Console.WriteLine("Informe a base:");
+abase = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe a altura:");
+altura = Convert.ToInt32(Console.ReadLine());
+
+resultado = abase * altura;
+
+Console.WriteLine("A área do retangulo corresponde a: " + resultado);
+
+Console.ReadLine();
